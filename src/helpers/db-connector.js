1f9e93c6d8839ebbf8f09f52036bbba85e29bd74
Rampradeep-k <rampradeepsamy@gmail.com>
2026-04-29 21:45:00 +0530

    env changes

--- src/helpers/db-connector.js
+++ src/helpers/db-connector.js
@@ -2,23 +2,24 @@ import { Pool } from 'pg';
 let pool; // Declare the pool outside of the try-catch block
 
 try {
-    pool = new Pool({
+    /* pool = new Pool({
         user: process.env.USER,
         host: process.env.HOST,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
-        /* ssl: {
+        ssl: {
             require: true,
             rejectUnauthorized: false,
-        }, */
-    });
-     /* pool=new Pool({
+        },
+    }); */
+    pool = new Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: {
             require: true,
+            rejectUnauthorized: false,  // ⭐ THIS FIXES IT
         },
-    });  */
+    });
 
     // console.log("Postgre Database Connected.");
 
